7321a31b593e0838f4a9eb61038c8ba428771d39
Simon Durbridge <durbridge123@googlemail.com>
2019-08-01 17:13:33 +0100

    base app
    Base app that can be used to monitor and record shimmer data, including heart rate estimation from PPG data

---
+++ prepShimmer.m
@@ -0,0 +1,5 @@
+function prepShimmer()
+javaaddpath('C:\Users\sedur\Downloads\Shimmer Matlab Instrument Driver v2.8\Shimmer Matlab Instrument Driver v2.8\ShimmerBiophysicalProcessingLibrary_Rev_0_10.jar');
+
+
+end
